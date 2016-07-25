81ddc79e6285cfa7b9bbb6b821e19e02d0dc7190
bravokeyl <bravokeyl@gmail.com>
2016-07-25 19:45:51 +0530

    Enqueue gimliii.css

--- functions.php
+++ functions.php
@@ -118,7 +118,7 @@ add_action( 'widgets_init', 'gimliii_widgets_init' );
 function gimliii_styles(){
 
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri().'/js/bootstrap/css/bootstrap.min.css', array() );
-	wp_enqueue_style( 'gimliii', get_stylesheet_uri(), array( 'bootstrap' ) );	
+	wp_enqueue_style( 'gimliii', get_template_directory_uri().'/css/gimliii.css', array( 'bootstrap' ) );	
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri().'/vendor/font-awesome/css/font-awesome.css', array() );  
 }
 
