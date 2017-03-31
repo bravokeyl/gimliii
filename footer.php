0e9250e6c5497dc503ac38e12a3a201bcf638bef
bravokeyl <bravokeyl@gmail.com>
2017-04-01 01:16:36 +0530

    Footer re

--- footer.php
+++ footer.php
@@ -1,40 +1,44 @@
-<?php 
+<?php
 if( is_active_sidebar('gimliii-footer-one') || is_active_sidebar('gimliii-footer-two') || is_active_sidebar('gimliii-footer-three') ){ ?>
 	<div class="footer">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4 md-margin-bottom-40">
-					<?php dynamic_sidebar('gimliii-footer-one');?>				
-				</div><!--/col-md-4-->	
-				
+					<?php dynamic_sidebar('gimliii-footer-one');?>
+				</div><!--/col-md-4-->
+
 				<div class="col-md-4 md-margin-bottom-40">
 	                <div class="posts">
 	                    <?php dynamic_sidebar('gimliii-footer-two');?>
 	                </div>
 				</div><!--/col-md-4-->
 
-				<div class="col-md-4">             
+				<div class="col-md-4">
 	                <?php dynamic_sidebar('gimliii-footer-three');?>
 				</div><!--/col-md-4-->
-			</div><!--/row-->	
-		</div><!--/container-->	
-	</div><!--/footer-->	
+			</div><!--/row-->
+		</div><!--/container-->
+	</div><!--/footer-->
 <?php }?>
 <div class="copyright">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6">						
-	            <p class="copyright-space">
-                <?php bloginfo('name');
-                	  _e(" | All Rights Reserved.","gimliii");
-                ?>
-                </p>
+			<div class="col-md-6">
+        <p class="copyright-space">
+          <?php
+						printf( '<a class="site-link" href="%s" rel="home">%s</a>', esc_url( home_url() ), get_bloginfo( 'name' ) );
+        	  _e( ' | All Rights Reserved.', 'gimliii' );
+          ?>
+        </p>
+			</div>
+			<div class="col-md-6 text-right">
+				<a href="<?php echo esc_url( __( 'https://mu-bit.com/', 'gimliii' ) ); ?>" title="With love by Mu Bit"> &#10084;</a>
 			</div>
 		</div><!--/row-->
-	</div><!--/container-->	
-</div><!--/copyright-->	
+	</div><!--/container-->
+</div><!--/copyright-->
 <?php wp_footer(); ?>
 
 </body>
 
-</html>	
+</html>
