eb28fe4c5053950333b8dc5f256706eb64384d07
bravokeyl <bravokeyl@gmail.com>
2017-04-01 01:56:26 +0530

    Post page title h1, h2

--- home.php
+++ home.php
@@ -1,11 +1,6 @@
 <?php get_header(); ?>
-<div class="breadcrumbs margin-bottom-40">
-    <div class="container">
-        <h1 class="pull-left"><?php _e('Home','gimliii');?></h1>
-    </div>
-</div>
-<div class="container">		
-	<div class="row blog-page margin-bottom-20">    
+<div class="container">
+	<div class="row blog-page margin-bottom-20">
     	<div class="col-md-9 md-margin-bottom-40">
             <?php
             $gimliii_post_class_home = " row blog blog-medium margin-bottom-40 ";
@@ -15,26 +10,26 @@
             <div id="post-<?php the_ID(); ?>" <?php post_class($gimliii_post_class_home); ?> >
                 <div class="col-md-5">
                     <?php if ( has_post_thumbnail() && ! post_password_required() ){ ?>
-                    <a href="<?php the_permalink(); ?>"> 
+                    <a href="<?php the_permalink(); ?>">
                             <?php   the_post_thumbnail('thumb-medium');
                             echo "</a>";
                     }else{
                         ?>
-                    
+
  <img src="<?php echo get_template_directory_uri(); ?>/img/default-thumb.jpg" alt="<?php the_title(); ?>" class="img-responsive"/>
                         <?php
                     }?>
 
-                </div>    
+                </div>
                 <div class="col-md-7">
                   <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>"> <?php the_title(); ?></a></h2>
                     <ul class="list-unstyled list-inline blog-info">
                         <li><i class="fa fa-calendar" aria-hidden="true"></i> <?php gimliii_posted_on();?></li>
                         <?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
-                        <li><i class="fa fa-comments" aria-hidden="true"></i> 
+                        <li><i class="fa fa-comments" aria-hidden="true"></i>
                         <?php comments_popup_link( __( 'Comment', 'gimliii' ), __( '1 comment', 'gimliii' ), __( '% Comments', 'gimliii' ) ); ?>
                     	</li><?php endif;?>
-                    	<li><i class="fa fa-copy" aria-hidden="true"></i> 
+                    	<li><i class="fa fa-copy" aria-hidden="true"></i>
                     		<?php
 			                 $catcountpost= sizeof(get_the_category());
 			                   if( $catcountpost>15){
@@ -44,13 +39,13 @@
 			                   }
 							   ?>
                     	</li>
-						<?php if(has_tag()){ ?><li><i class="fa fa-tags" aria-hidden="true"></i> <?php the_tags();?></li><?php } ?>	
+						<?php if(has_tag()){ ?><li><i class="fa fa-tags" aria-hidden="true"></i> <?php the_tags();?></li><?php } ?>
                     </ul>
                     <p><?php the_excerpt(); ?></p>
                     <p><a class="btn-u btn-u-small" href="<?php the_permalink(); ?>"><i class="fa fa-eye" aria-hidden="true"></i><?php _e('Read More','gimliii');?></a></p>
-                </div>    
+                </div>
             </div>
-            <!--End Blog Post-->        
+            <!--End Blog Post-->
 
             <hr class="margin-bottom-40">
             	<?php endwhile;
@@ -59,7 +54,7 @@
 		?>
         </div>
     	<?php get_sidebar();?>
-    </div><!--/row-->        
-</div><!--/container-->		
+    </div><!--/row-->
+</div><!--/container-->
 
 <?php get_footer(); ?>
