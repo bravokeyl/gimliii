0aff99a8487107cea05a7a4ee2b28660d48d6e0f
bravokeyl <bravokeyl@gmail.com>
2017-03-31 23:20:58 +0530

    Translation

--- inc/widgets.php
+++ inc/widgets.php
@@ -8,7 +8,7 @@ class gimliii_posts_list extends WP_Widget {
 	public function __construct() {
 		$widget_ops = array( 'classname' => 'posts-list'  );
 		$control_ops = array( 'width' => 250, 'height' => 350, 'id_base' => 'posts-list-widget' );
-		parent::__construct('posts-list-widget', __('Gimli Posts list'), $widget_ops, $control_ops);
+		parent::__construct('posts-list-widget', __('Gimli Posts list', 'gimliii'), $widget_ops, $control_ops);
 	}
 
 	public function widget( $args, $instance ) {
@@ -85,7 +85,7 @@ class gimliii_search extends WP_Widget {
 	public function __construct() {
 		$widget_ops = array( 'classname' => 'search'  );
 		$control_ops = array( 'width' => 250, 'height' => 350, 'id_base' => 'search-widget' );
-		parent::__construct('search-widget', __('Gimli Search'), $widget_ops, $control_ops);
+		parent::__construct('search-widget', __('Gimli Search', 'gimliii'), $widget_ops, $control_ops);
 	}
 	public function widget( $args, $instance ) { ?>
 	<div class="lb lb-md"><h2><?php _e('Search','gimliii');?></h2></div>
